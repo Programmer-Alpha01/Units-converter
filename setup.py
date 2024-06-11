10a4654ce60f82f8b2be84c5fd427e9b9f51e65a
冰水不冰 <icewaternotice@gmail.com>
2024-06-12 02:19:25 +0800

    made a setup.py file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='Units-converter', #same as repository name
+   version='1.0',
+   description='A useful Units converter',
+   license="MIT",
+   long_description=long_description,
+   author='',
+   author_email='',
+   url="",
+   packages=['Units-converter'],  #same as name
+   install_requires=[], #external packages as dependencies
+   scripts=[
+            'scripts/distance',
+           ]
+)
