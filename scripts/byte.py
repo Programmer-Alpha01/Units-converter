83e4d449f52a80b91601294ce07bb8e3cc4088e4
Programmer-Alpha01 <139827442+Programmer-Alpha01@users.noreply.github.com>
2024-06-17 13:42:11 +0000

    byte

--- scripts/byte.py
+++ scripts/byte.py
@@ -1,3 +1,3 @@
 def byte(From, To, Value):
     byte=['b','kb','mb','gb','tb','pb','eb','eb','zb','yb']
-    return unit * (1024 ** (byte.index(From)-byte.index(To)))
+    return Value * (1024 ** (byte.index(From)-byte.index(To)))
