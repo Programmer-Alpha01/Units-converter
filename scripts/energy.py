66457546d3850998c897867fb4af8e22fa585926
Programmer-Alpha01 <139827442+Programmer-Alpha01@users.noreply.github.com>
2024-06-21 15:21:46 +0000

    energy

---
+++ scripts/energy.py
@@ -0,0 +1,13 @@
+def energy(From,To,value):
+    energy={
+        'j':1,                  # Joule
+        'kj':0.001,             # Kilo Joule
+        'kwh':3.6*(10**6),      # Kilowatt-hour
+        'cal':4.184,            # Calorie
+        'btu':1055,             # British Thermal Unit
+        'quad':1055*(10**15),   # Quad
+        'therm':100000*1055,    # Therm
+        'mcf':10.27*100000*1055 # One thousand cubic feet of gas
+    }
+
+    return value * energy[From] / energy[To]
