3217a3dc59a0da182cb203f621a75528afc9f28c
Programmer-Alpha01 <139827442+Programmer-Alpha01@users.noreply.github.com>
2024-07-05 00:11:52 +0800

    Update force.py

--- scripts/force.py
+++ scripts/force.py
@@ -2,7 +2,7 @@ def force(From, To, Value):
     force = { 
         "N": 1, # Newton
 
-        "dyn": 1*10**-5, # dyne
+        "dyne": 1*10**-5, # dyne
         "kp": 9.80665, # kilopond
         "lbf": 4.44822, # pound-force
         "pdl": 0.138255, # poundal
