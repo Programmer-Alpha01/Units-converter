a6798f579a54e7e5fe1ee6c0c1dd63a577de41ac
Programmer-Alpha01 <139827442+Programmer-Alpha01@users.noreply.github.com>
2024-07-05 00:52:06 +0800

    Update pressure.py

--- scripts/pressure.py
+++ scripts/pressure.py
@@ -20,18 +20,8 @@ def pressure(From, To, Value):
         "cmH2O" : 98.0665, # centimetre of water
         "mmH2O" : 9.80665, # millimetre of water
 
-        # Copper units of pressure
-        "cPa" : 1.33322, # centipascal
-        "mPa" : 133.322, # millipascal
-        "uPa" : 133.322, # micropascal
-        "nPa" : 133.322, # nanopascal
-        "pPa" : 133.322, # picopascal
-        "fPa" : 133.322, # femtopascal
-        "aPa" : 133.322, # attopascal
-        "daPa" : 133.322, # decapascal
-        "hPa" : 1333.22, # hectopascal
-        "kPa" : 13332.2, # kilopascal
-        "MPa" : 133322000, # megapascal
+
+        
         'planck': 4.63309*(10**113)
         }
     
